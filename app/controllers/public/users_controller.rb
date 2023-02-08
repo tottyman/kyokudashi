7fbc3e7cdbb6484761d32c1f69beaa2278ff040a
EC2 Default User <ec2-user@ip-172-31-43-201.ap-northeast-1.compute.internal>
2023-02-08 08:02:16 +0000

    修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,7 @@
 class Public::UsersController < ApplicationController
 
   before_action :authenticate_user!, except: [:index]
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.page(params[:page])
@@ -36,4 +37,11 @@ class Public::UsersController < ApplicationController
     params.require(:user).permit(:name, :nickname, :email, :password, :introduction, :image)
   end
 
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+  
 end
